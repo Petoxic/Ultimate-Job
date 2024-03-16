5ec2895aa055d57efcf0020cddc75857cf497924
Bugguga <nat14660@hotmail.com>
2024-03-16 23:28:52 +0700

    create scene

---
+++ Assets/ResultScreen.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultScreen : MonoBehaviour
+{
+    public void Setup(int[] objectives)
+    {
+        gameObject.SetActive(true);
+    }
+}
