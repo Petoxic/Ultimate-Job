517e98b86e8f7928f1480641aca45de71f862be6
Pattanan Piyachotinan <70986870+Petoxic@users.noreply.github.com>
2024-03-16 22:44:12 +0700

    Add food type and dictionary

---
+++ Assets/FoodType.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FoodType
+{
+    STEAK,
+    CHICKEN,
+    EGG,
+    BOILED_EGG,
+    FRIED_EGG,
+    TURKEY,
+    DOUBLE_CHICKEN,
+    SUSHI_ROLL_1,
+    SUSHI_ROLL_2,
+    SUSHI_SHRIMP,
+    SUSHI_TUNA,
+    SUSHI_SALMON,
+    SUSHI_EGG,
+    SUSHI_SHELL,
+    SUSHI_EEL,
+    CUPCAKE,
+    DONUT
+}
