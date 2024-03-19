862ba43300afa88d8d54d04eff82df99a51c2043
GustPhitchaya <77724168+GustPhitchaya@users.noreply.github.com>
2024-03-19 22:40:04 +0700

    Remove logs and comments

--- Assets/Script/KitchenScript.cs
+++ Assets/Script/KitchenScript.cs
@@ -27,7 +27,6 @@ public class KitchenScript : MonoBehaviour
 
     void Update()
     {
-        Debug.Log("order buffer " + orderBuffer);
         Debug.Log("order buffer count " + orderBuffer.Count);
         if (orderBuffer.Count > 0 && !timerSlider.gameObject.activeSelf && !IsServingSlotFull())
         {
