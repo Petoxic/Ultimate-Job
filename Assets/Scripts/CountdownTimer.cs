f6fb2f45311962a2f0b3db9a11ee1b36ce8fdf44
ing <88963106+ingthanhunt@users.noreply.github.com>
2024-04-14 14:18:00 +0700

    Add countdown progress bar

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CountdownTimer : MonoBehaviour
 {
+    public Image timer_progress_bar;
     private float timeWhenSceneEnds;
+    private float max_time;
 
     void Start()
     {
         timeWhenSceneEnds = DataManager.timeUntilSceneEnds;
+        max_time = DataManager.timeUntilSceneEnds;
     }
 
     void Update()
@@ -17,6 +21,7 @@ public class CountdownTimer : MonoBehaviour
         if (!DataManager.startTalking)
         {
             timeWhenSceneEnds -= Time.deltaTime;
+            timer_progress_bar.fillAmount = timeWhenSceneEnds / max_time;
             if (timeWhenSceneEnds <= 0)
             {
                 DataManager.isGameEnd = true;
